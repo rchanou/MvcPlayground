3b51528acb62405c0aea151aefcdb957612e8fdd
rchanou <rchanou@gmail.com>
2014-08-26 22:17:27 -0700

    Added simple testing harness

--- MvcPlayground/Controllers/TestController.cs
+++ MvcPlayground/Controllers/TestController.cs
@@ -3,18 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcPlayground.Models;
 
 namespace MvcPlayground.Controllers
 {
-    public class TestController : Controller
+    [TestClass]
+    public class TestController : HomeController
     {
         //
         // GET: /Test/
 
-        public ActionResult Index()
+        [TestMethod]
+        public void Testing_Works()
         {
-            return View();
+            Assert.AreEqual(1+1, 2);
         }
 
+        [TestMethod]
+        public void BoundRecordInfo_Construction_Succeeds()
+        {
+            var customer = new Customer {ID = 15};
+            var boundRecord = new BoundRecordInfo(customer);
+            Assert.AreEqual(boundRecord.entityClass, "MvcPlayground.Models.Customer");
+            Assert.AreEqual(boundRecord.primaryKey, 15);
+        }
     }
 }
